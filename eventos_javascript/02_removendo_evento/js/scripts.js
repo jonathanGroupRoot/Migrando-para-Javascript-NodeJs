a1b29a2e9afcc81bacbd3dc338e909f08d57c7e4
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-04-18 22:24:49 -0300

    Inserindo Arquivos JS

---
+++ eventos_javascript/02_removendo_evento/js/scripts.js
@@ -0,0 +1,48 @@
+let but = document.querySelector('#btn');
+let msg = function()
+{
+    console.log('Deu Certo');
+}
+but.addEventListener('click',msg);
+
+let but2 = document.querySelector('#btn2');
+
+but2.addEventListener('click',function()
+{
+    but.removeEventListener('click',msg);
+});
+
+
+
+
+let alerta = document.querySelector('#alerta');
+let msg2 = function()
+{
+    console.log('Testando Alert');
+};
+alerta.addEventListener('click',msg2);
+
+let cons = document.querySelector('#console');
+
+
+let msg3 = function()
+{
+    console.log('Imprimindo no console');
+};
+cons.addEventListener('click',msg3);
+
+let removerAlerta = addEventListener('click', function()
+{
+    cons.removeEventListener('click',msg3)
+});
+
+let buscaCor = document.getElementById('#calor');
+buscaCor = addEventListener('click', function()
+{   
+    let calor = document.getElementById('calor2');
+    calor.style.color = 'red';
+    calor.style.fontSize = '50px';
+});
+
+
+
